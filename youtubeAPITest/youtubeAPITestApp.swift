4c885f0da406843a836819dfba09f0b52351c3a8
j-laiti <justinlaiti22@rcsi.ie>
2023-09-14 21:32:49 +0100

    Initial Commit

---
+++ youtubeAPITest/youtubeAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeAPITestApp.swift
+//  youtubeAPITest
+//
+//  Created by Justin Laiti on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct youtubeAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
